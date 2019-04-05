547191b0125b4e7e9d9a5011e5e36c506fe5fd04
Subaru-Arakawa <subaru@takepartsfactory.com>
2019-04-06 06:57:49 +0900

    Initial Commit

---
+++ smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smack
+//
+//  Created by Subaru Arakawa on 2019/04/06.
+//  Copyright © 2019 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
